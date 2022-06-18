1509afaaab6588df271a96978414975ad09541ba
Gabriel-ds <gabrields1910@gmail.com>
2022-06-18 18:48:29 -0300

    Criação da tela de inserção de dados

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="center">Calcule o seu IMC</h1>
+  <AddImc />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddImc from "./components/AddImc.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AddImc,
+  },
+};
 </script>
 
 <style>
